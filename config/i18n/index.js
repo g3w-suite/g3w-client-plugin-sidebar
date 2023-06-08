b04322236e13bca81941f3530df3905926caf660
volterra79 <boccacci.francesco@gmail.com>
2023-06-08 10:56:15 +0200

    :globe_with_meridians: Create single language files

--- config/i18n/index.js
+++ config/i18n/index.js
@@ -1,9 +1,17 @@
+import it from './it';
+import en from './en';
+import fi from './fi';
+import se from './se';
+import fr from './fr';
+import de from './de';
+import ro from './ro';
+
 export default {
-  it: {},
-  en: {},
-  fi: {},
-  se: {},
-  fr: {},
-  de: {},
-  ro: {}
+  it,
+  en,
+  fi,
+  se,
+  fr,
+  de,
+  ro,
 }
